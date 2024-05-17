6885336914a60ef95a82e8504f26a969e802dd95
Larry Tucker <larrydeontucker@gmail.com>
2024-05-17 10:48:15 -0700

    Built base functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Number Range
+
+Function that builds an array of numbers from the given start and end numbers.
--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-/*
- * Number Range
- *
- * Build an array of numbers from a given start and end number.
- * 
- */
+function range(start, end) {
+    let numbers = [start];
+
+    for(let step = 0; step <= end; step++) {
+        let next = numbers[step] + 1;
+        numbers.push(next);
+
+        if(next == end) {
+            break;
+        }
+    }
+
+    return numbers;
+}
+
+console.log(range(1, 5));
+console.log(range(-5, -1));
 
